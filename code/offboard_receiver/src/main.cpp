220f76e07fb49bfee2263fee804622aabfdbcf4c
zhksh <pascal@guldener.io>
2023-03-19 23:53:40 +0100

    weljnwerf

--- code/offboard_receiver/src/main.cpp
+++ code/offboard_receiver/src/main.cpp
@@ -8,31 +8,30 @@
 #define PREFIX 0xd0000000  //prefix für empfang der nachricht
 #define PREFIX_MASK 0x0fffffff
 
-// #define TB_MASK ((1 << 10) - 1) << 17
-#define TB_MASK    0b00000011111111100000000000000000
-#define TEMP0_MASK 0b00000000000000011111111000000000
+#define TB_MASK    0x0fff0000
+#define TB_POS     16
 
+#define TEMP0_MASK 0x0000ff00
+#define TEMP0_POS  8
 
+#define TEMP1_MASK 0x000000ff
+#define TEMP1_POS  0
 
-
-int baud = 9600;
-
+#define BAUD  9600
 
 RCSwitch mySwitch = RCSwitch();
 
 void setup() {
-  Serial.begin(baud);
+  Serial.begin(BAUD);
   Serial.println("Receiver setup");
   mySwitch.enableReceive(RECEIVER_PIN);
 }
 
-
 long decode(long msg) {
-  //this conversion cuts off the highest two bytes
   return msg & PREFIX_MASK;
 }
 
-bool checkPrefix(unsigned long original_msg, int decoded) {
+bool checkPrefix(unsigned long original_msg, long  decoded) {
   bool passed = (decoded | PREFIX) == original_msg;
   // Serial.print("msg");
   // Serial.print(original_msg);
@@ -47,32 +46,27 @@ void debug(RCSwitch s){
 void loop() {
   if (mySwitch.available()) {  // Wenn ein Code Empfangen wird...
     debug(mySwitch);
-
     unsigned long msg = mySwitch.getReceivedValue();
-    long code = decode(msg);    
-    Serial.println((msg & TB_MASK) >> 16);
-    Serial.println((msg & TEMP0_MASK) >> 8);
+    long decoded = decode(msg);  
 
-
-    bool legit = checkPrefix(msg, code);
+    bool legit = checkPrefix(msg, decoded);
     if (legit) {
-      Serial.print("Legit Code: ");
-      Serial.println(code);
+      // Serial.print("Legit Code: ");
+      // Serial.println(code);
+      long tb_val =  (msg & TB_MASK) >> TB_POS;
+      long temp0_val = (msg & TEMP0_MASK) >> TEMP0_POS;
+      long temp1_val = (msg & TEMP1_MASK);
 
-      // schalter1 = code % 2; //most right bit
-      // schalter2 = (code - schalter1) / 2 % 2; //second most right bit
-      // schalter3 = (code - 2 * schalter2 - schalter1) / 4 % 2; //third most right bit
-
-      // joystick0 = (code - 4 * schalter3 - 2 * schalter2 - schalter1) / 8 % 1024; 
-      // joystick1 = (code - 8 * joystick0 - 4 * schalter3 - 2 * schalter2 - schalter1) / 8192;
-
+      Serial.println(tb_val);
+      Serial.println(temp0_val);
+      Serial.println(temp1_val);
     }
-   else {
-    Serial.print("Noise: ");
-    Serial.println(code);
+    else {
+      Serial.print("Noise: ");
+      // Serial.println(code);
+    }
+
+
+    mySwitch.resetAvailable();
   }
-
-
-  mySwitch.resetAvailable();  // Hier wird der Empfänger "resettet"
-}
 }
