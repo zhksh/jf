2622c78af3a4bf4de6cd753dcc3d57900b8d1c75
Pascal Guldener <pascal@guldener.io>
2023-03-29 14:43:55 +0200

    refactored data structs

--- code/onboard_receiver/src/util.h
+++ code/onboard_receiver/src/util.h
@@ -1,6 +1,14 @@
 #include <RCSwitch.h>
 
 #define CHECK_BIT(var,pos) ((var) & (1<<(pos))) != 0
+#define BIT_RANGE(var, len, pos) (var >> pos) & ((1 << len)-1)
+
+long bitrange(long msg, int len, int pos){
+  long mask = (1 << len)-1;
+  // Serial.println(mask);
+  long result = msg >> pos;
+  return result & mask;
+}
 
 struct XAxis {
   bool right;
@@ -14,106 +22,201 @@ struct YAxis {
   bool none;
 };
 
-struct JData {
-  XAxis x;
-  YAxis y;
-};
-
-struct JCD {
-  JData j0;
-  JData j1;
-};
-
-long bitrange(long msg, int len, int pos){
-  long mask = (1 << len)-1;
-  // Serial.println(mask);
-  long result = msg >> pos;
-  return result & mask;
-}
-
-
-
-JCD readJSData(int data){
-  JCD cd = {};
-  // cd.j1.x.none = (data & 0b01000000) >> 6;
-  // cd.j0.x.right = (data & 0b10000000) >> 7;
-  // cd.j0.x.left = 1-((data & 0b10000000) >> 7);
-
-  // cd.j0.y.none = (data & 0b00010000) >> 4;
-  // cd.j0.y.up = (data & 0b00100000) >> 5;
-  // cd.j0.y.down = 1-((data & 0b00100000) >> 5);
-
-
-  // cd.j1.x.none = (data & 0b00000100) >> 2;
-  // cd.j1.x.right = (data & 0b00000010) >> 3;
-  // cd.j1.x.left = 1-((data & 0b00000010) >> 3);
-
-  // cd.j1.y.none = (data & 0b00000001);
-  // cd.j1.y.up = (data & 0b00000010) >> 1;
-  // cd.j1.y.down = 1-((data & 0b00000010) >> 1);
-
-  cd.j1.x.none = CHECK_BIT(data, 6);
-  cd.j0.x.right = CHECK_BIT(data, 7);
-  cd.j0.x.left = !cd.j0.x.right;
-
-  cd.j0.y.none = CHECK_BIT(data, 4);
-  cd.j0.y.up = CHECK_BIT(data, 5);
-  cd.j0.y.down = !cd.j0.y.up;
-
-
-  cd.j1.x.none = CHECK_BIT(data, 2);
-  cd.j1.x.right = CHECK_BIT(data, 3);
-  cd.j1.x.left = !cd.j1.x.right; 
-
-  cd.j1.y.none = (data & 0b00000001);
-  cd.j1.y.up = CHECK_BIT(data, 1);
-  cd.j1.y.down = !cd.j1.y.up;
-
-
-  return cd;
-};
-
-
-void debugJParsedConfig(JCD jcd){
-  String r = "rechts";
-  String l = "links";
-  String h = "hoch";
-  String ru = "runter";
-  String n = "neutral";
-  Serial.print("J0 ");
-  Serial.print("x: ");
-  if (jcd.j0.x.none){
-    Serial.print(n); 
-  } 
-  else {
-      if (jcd.j0.x.right) Serial.print(r);
-      else if (jcd.j0.x.left) Serial.print(l);
-  }
-  Serial.print(" y: ");
-  if (jcd.j0.y.none) Serial.print(n);
-  else {
-    if (jcd.j0.y.up) Serial.print(h);
-    else if(jcd.j0.y.down) Serial.print(ru);
-  }
+class Joystick {
+  public:
+    XAxis x;
+    YAxis y;
   
-  Serial.println("");
-  Serial.print("J1:");
-  Serial.print(" x: ");
-  if (jcd.j1.x.none) Serial.print(n);
-  else {
-      if (jcd.j1.x.right) Serial.print(r);
-      else if (jcd.j1.x.left) Serial.print(l);
-
+  String name;
+  
+  Joystick(int data){
+    parse(data);
   }
-  Serial.print(" y: ");
-  if (jcd.j1.y.none) Serial.print(n);
-  else {
-    if (jcd.j1.y.up) Serial.print(h);
-    else if (jcd.j1.y.down) Serial.print(ru);
 
+  Joystick(String desc){
+    setName(desc);
+  };
+
+  void parse(int data){
+    int xData = bitrange(data, 2, 0);
+    int right = CHECK_BIT(xData, 1);
+    x = XAxis{right, !right, CHECK_BIT(xData, 0)};
+
+    int yData = bitrange(data, 2, 2);
+    int up = CHECK_BIT(yData,1);
+    y = YAxis{up, !up, CHECK_BIT(yData, 0)};
   }
-      Serial.println("");
-    // Serial.print("data:");
-    // Serial.print(data);
-    // Serial.println("");
+
+  void setName(String descr){
+    name = descr;
+  }
+
+};
+
+
+long decode(long msg, long prefix_mask) {
+  return msg & prefix_mask;
 }
+
+bool islegit(unsigned long original_msg, unsigned long  decoded, long prefix) {
+  return (decoded | prefix) == original_msg;
+}
+
+class TZ {
+    public: 
+    bool stop = false;
+    bool ausgefahren = false;
+    bool faus = false;
+    bool fein = false;
+    unsigned long ausz = 0;
+    unsigned long einz = 0;
+    unsigned long counter = 0;
+    int speedPin = 150;
+
+    int pin1;
+    int pin2;
+    int stopPin;
+
+    String name;
+
+    TZ(unsigned long afz, unsigned long efz, int p1, 
+        int p2, int stop_pin, int speed_pin, String descr){
+      ausz = afz;
+      einz = efz;
+      pin1 = p1;
+      pin2 = p2;
+      stopPin = stop_pin;
+      speedPin = speed_pin;
+      name = descr;
+    }
+
+    void setSpeed(int speed){
+      analogWrite(speedPin, speed);
+    }
+
+    void turnR(){
+      setPins(LOW, HIGH);
+      einz = true;
+      log("einfahren");
+      Serial.print(millis() - counter);
+      Serial.print(")");
+    };
+
+    void turnL(){
+      setPins(HIGH, LOW);
+      einz = true;
+      log("ausfahren");
+      Serial.print((millis() - counter));
+      Serial.print(")");
+    };
+
+    void stopTZ(){
+      setPins(LOW, LOW);
+      einz = false;
+      ausz = false;
+      counter = 0;
+     log("stop");
+    }
+
+    void log(String action){
+      Serial.print(name);
+      Serial.print(":");
+      Serial.print(action);
+    };
+
+    void setPins(int p1, int p2){
+      digitalWrite(pin1, p1);
+      digitalWrite(pin2, p2);
+    };
+
+    void update(int controlSig){
+      if (digitalRead(stopPin) == 0){
+        ausgefahren = true;
+        stopTZ();
+      }
+      else {
+        if (controlSig) {  
+          if (!ausgefahren){
+            if ((counter > 0) && (millis() - counter) >= ausz){
+              stopTZ();
+              ausgefahren = true;
+            }
+            else {
+              //tauchzelle fährt nicht gerade aus
+              if (!fein){
+                if (!faus){
+                  //start timer ausfahrt
+                  counter = millis();
+                }
+                turnL();      
+              }       
+            }         
+          }     
+        }
+        //schalter aus
+        else {
+          //nur wenn ganz ausgefahren, wieder einfahren
+          if (ausgefahren){
+            if (!fein){
+              //start timer einfahrt
+              counter = millis();
+            }
+            if ((counter > 0) && (millis() - counter) > einz){
+              //genug eingefahren, stop
+              log("eingefahren");
+              stopTZ();
+              ausgefahren = false;
+            }
+            else {
+              //einfahren
+              turnR();
+            }       
+          }      
+        }
+      }
+    }
+};
+
+// void debugJParsedConfig(JCD jcd){
+//   String r = "rechts";
+//   String l = "links";
+//   String h = "hoch";
+//   String ru = "runter";
+//   String n = "neutral";
+//   Serial.print("J0 ");
+//   Serial.print("x: ");
+//   if (jcd.j0.x.none){
+//     Serial.print(n); 
+//   } 
+//   else {
+//       if (jcd.j0.x.right) Serial.print(r);
+//       else if (jcd.j0.x.left) Serial.print(l);
+//   }
+//   Serial.print(" y: ");
+//   if (jcd.j0.y.none) Serial.print(n);
+//   else {
+//     if (jcd.j0.y.up) Serial.print(h);
+//     else if(jcd.j0.y.down) Serial.print(ru);
+//   }
+  
+//   Serial.println("");
+//   Serial.print("J1:");
+//   Serial.print(" x: ");
+//   if (jcd.j1.x.none) Serial.print(n);
+//   else {
+//       if (jcd.j1.x.right) Serial.print(r);
+//       else if (jcd.j1.x.left) Serial.print(l);
+
+//   }
+//   Serial.print(" y: ");
+//   if (jcd.j1.y.none) Serial.print(n);
+//   else {
+//     if (jcd.j1.y.up) Serial.print(h);
+//     else if (jcd.j1.y.down) Serial.print(ru);
+
+//   }
+//       Serial.println("");
+//     // Serial.print("data:");
+//     // Serial.print(data);
+//     // Serial.println("");
+// }
